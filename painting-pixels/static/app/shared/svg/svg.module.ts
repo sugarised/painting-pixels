dc3fdaacd02122bbb4406c7fdea517275aef6bf8
Leah <leah@iMcBeast.fubarnet>
2022-01-22 17:31:44 +0000

    linting working

---
+++ painting-pixels/static/app/shared/svg/svg.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+
+import {
+  SVGPaintComponent
+} from './svg.component';
+
+@NgModule({
+  declarations: [
+    SVGPaintComponent
+  ],
+  exports: [
+    SVGPaintComponent
+  ],
+})
+export class SvgModule {}
